74bb4e919a9dbe61d01f49877e2be703e695a036
Price47 <Richardstoeffel47@gmail.com>
2017-05-19 00:08:32 -0400

    Update javascript, add loader examples

--- priceweb/static/javascript/control.js
+++ priceweb/static/javascript/control.js
@@ -3,14 +3,70 @@
  */
 
 $(document).ready(function(){
+    $('#carousel').carousel();
 
-    $('#home-logo a img').hover(
+    var loaderExample = 1;
+
+    setTimeout( function(){
+        $('.loader').css("display", "none")
+    },1000 );
+
+
+    function highlight(selector){
+        selector.removeClass('home-links-still');
+        selector.addClass('gradient1');
+    }
+
+    function delight(selector){
+        selector.removeClass('gradient1');
+        selector.addClass('home-links-still');
+    }
+
+    $('.home-logo a img').hover(
         function(){
-            $(this).addClass('spin')
+            $(this).addClass('spin');
+            $('#title_main').text('Home')
     },
         function(){
-            $(this).removeClass('spin')
-        })
+            $(this).removeClass('spin');
+            $('#title_main').text('')
+        }
+    );
+
+    $('#about_link, #about_button').hover(
+        function(){
+            $('#title_main').text('About This Page');
+        },
+        function(){
+            $('#title_main').text('')
+
+        }
+    );
+
+    $('#dori_link, #dori_button').hover(
+        function(){
+            $('#title_main').text('DORi Development Page');
+        },
+        function(){
+            $('#title_main').text('');
+        }
+    );
+
+    $('#loadscreen_button').hover(
+        function(){
+            $('#title_main').text('Loading Screens');
+        },
+        function(){
+            $('#title_main').text('');
+        }
+    );
+
+    $('#carousel-slide-caption').html($('.active > .carousel-caption').html());
+    $('.carousel').on('slid.bs.carousel', function () {
+        $('#carousel-slide-caption').html($('.active > .carousel-caption').html());
+        });
 
 });
+
+
 
--- priceweb/urls.py
+++ priceweb/urls.py
@@ -3,6 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='index'),
     url(r'^about$', views.about, name='about'),
+    url(r'^dori', views.dori, name='dori'),
+    url(r'^home', views.home, name='home'),
+    url(r'^loadingscreens/(?P<index>[0-9]{1})' , views.loadingscreens, name='loadingscreens'),
+    url(r'^test', views.test, name='test'),
+
 ]
