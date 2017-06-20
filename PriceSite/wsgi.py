d032c9fa141265830a551b0a775b35cfba864d87
Price <rockstar645@Richards-MacBook-Pro.local>
2017-06-20 17:07:13 -0400

    add sys path to wsgi

--- PriceSite/wsgi.py
+++ PriceSite/wsgi.py
@@ -11,7 +11,7 @@ import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-#sys.path.insert(0, '/opt/python/current/app')
+sys.path.insert(0, '/opt/python/current/app')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PriceSite.settings")
 
